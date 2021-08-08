4f7dd310e91022e713f890c62bd1c3e48dcacdf9
Odeh <abuzaid163@hotmail.com>
2021-08-08 16:57:43 +0300

    lab06-allFeaturesapplied

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Container, Col, Row, Alert } from 'react-bootstrap';
+import { Container, Col, Row, Alert, Toast } from 'react-bootstrap';
 
 import axios from 'axios';
 import Header from './Header';
@@ -10,11 +10,15 @@ export default class Main extends Component {
     super( props );
     this.state = {
       searchQuery: '',
+      locationData: {},
+      showMap: true,
       lat: 0,
       lon: 0,
       display_name: '',
-      showMap: true,
+      errorAlert: false,
+      showToast: false,
       errorMsg: '',
+
     };
   }
 
@@ -44,37 +48,50 @@ export default class Main extends Component {
 
   error = ( err ) => {
     <Alert variant='danger'>
-      `ERROR(${err.code}): ${err.message} \n ${this.state.errorMsg}`
+      `ERROR(${err.code}): ${err.message}`
     </Alert>;
+    // console.warn();
   };
 
   getData = async ( e ) => {
     e.preventDefault();
     await this.setState( { searchQuery: e.target.searchText.value } );
+
     let searchURL = `https://eu1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.searchQuery}&format=json`;
     try {
       let axiosResponse = await axios.get( searchURL );
+      // console.log( axiosResponse );
       await this.setState( {
         showMap: true,
         lat: axiosResponse.data[0].lat,
         lon: axiosResponse.data[0].lon,
         display_name: axiosResponse.data[0].display_name,
+        showToast: false,
       } );
     } catch ( error ) {
       await this.setState( {
         errorMsg: error.response.status + ' the location not found',
+        showToast: true,
       } );
     }
   };
 
   getCity = async ( long,lat ) => {
-    //Revese LookuppPppP
+    // reverse Lookup << :)
     let searchURL =
       `https://us1.locationiq.com/v1/reverse.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&lat=${lat}&lon=${long}` +
       '&format=json';
     let axiosResponse = await axios.get( searchURL );
+    //// console.log(axiosResponse)
     await this.setState( {
       display_name: axiosResponse.data.display_name,
+    } );
+
+  };
+
+  hideToast = () => {
+    this.setState( {
+      showToast: false,
     } );
   };
 
@@ -84,8 +101,26 @@ export default class Main extends Component {
         <Header getData={this.getData} />
         <main>
           <Container fluid>
-            <Row className='justify-content-md-center mt-4'>
-              <Col sm={4}>
+            <Row className='justify-content-md-center mb-4'>
+              {this.state.showToast && (
+                <Toast
+                  key='1'
+                  style={{ width: '100%' }}
+                  delay={1000}
+                  show={this.state.showToast}
+                  onClick={this.hideToast}
+                >
+                  <Toast.Header closeButton={false}>
+                    <strong className='me-auto'>ERROR</strong>
+                  </Toast.Header>
+                  <Toast.Body>
+                    Cannot get data : {this.state.errorMsg}
+                  </Toast.Body>
+                </Toast>
+              )}
+            </Row>
+            <Row className='justify-content-md-center mb-4'>
+              <Col sm={5}>
                 {this.state.showMap && (
                   <Map
                     cityName={this.state.display_name}
@@ -94,6 +129,7 @@ export default class Main extends Component {
                     lon={this.state.lon}
                   />
                 )}
+
               </Col>
             </Row>
           </Container>
